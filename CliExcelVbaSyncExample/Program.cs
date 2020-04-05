fcf53f68f3b36009f572e6ac8278fb9120371a3d
Hector <dev@hsmith.org>
2020-04-05 09:37:24 +0100

    Add example CLI project
    Add an example CLI project to demonstrate how to export VBA code from an
    excel workbook.
    Fix dependencies in the main library to reference the correct library.
    
    Issue #1.

---
+++ CliExcelVbaSyncExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExcelVbaSync.Api;
+using Microsoft.Office.Interop.Excel;
+using System;
+
+namespace CliExcelVbaSyncExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workbookName = args[0];
+            string outputDirectory = args[1];
+
+            Application app = new Application();
+            Workbook workbook = app.Workbooks.Open(workbookName);
+
+            ExcelVbaSyncApi.Instance.NewVbaExporter(workbook, outputDirectory).Export();
+
+            workbook.Close(false);
+            app.Quit();
+        }
+    }
+}
