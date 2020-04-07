e520b5164a69a480eb1ad713cdeb00403a5e7034
Hector <dev@hsmith.org>
2020-04-07 19:09:49 +0100

    Improve command processing in example CLI tool
    Add dependency on the CommandLineParser library in the example CLI project
    to allow better processing of command line arguments. Two options are now
    supported: Import and export.
    
    Issue #2

---
+++ CliExcelVbaSyncExample/ImportOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CliExcelVbaSyncExample
+{
+    [Verb("import", HelpText = "Import VBA code from a directory into an Excel Workbook file")]
+    class ImportOptions
+    {
+        [Option('s', "source", Required = true, HelpText = "Path to directory that contains exported VBA code files")]
+        public string SourceDirectoryPath { get; set; } = "";
+
+        [Option('t', "target", Required = true, HelpText = "Path to the Excel Workbook to import the VBA code into")]
+        public string TargetWorkbookPath { get; set; } = "";
+    }
+}
