6ad974b1f7c679da12129966db77b3de72ffd543
Hector <hector@hjs.dev>
2022-03-30 17:07:43 +0100

    docs: add documentation for all public code
    Update all the various public interfaces and classes to include code docs
    to facilitate using the library.

--- ExcelVbaSync/Api/IExcelVbaSyncApi.cs
+++ ExcelVbaSync/Api/IExcelVbaSyncApi.cs
@@ -5,10 +5,19 @@ using Microsoft.Office.Interop.Excel;
 
 namespace ExcelVbaSync.Api
 {
+    /// <summary>
+    /// Main entrypoint to the library.
+    /// </summary>
     public interface IExcelVbaSyncApi
     {
+        /// <summary>
+        /// Build a new instance of <see cref="IExcelVbaExporter"/> for the provided workbook.
+        /// </summary>
         IExcelVbaExporter NewVbaExporter(Workbook workbook);
 
+        /// <summary>
+        /// Build a new instance of <see cref="IExcelVbaImporter"/> for the provided workbook.
+        /// </summary>
         IExcelVbaImporter NewVbaImporter(Workbook workbook);
     }
 }
--- ExcelVbaSync/Vba/VbComponentType.cs
+++ ExcelVbaSync/Vba/VbComponentType.cs
@@ -4,15 +4,33 @@ using System.Text;
 
 namespace ExcelVbaSync.Vba
 {
+    /// <summary>
+    /// Pseudo-enum type containing all the different types of <see cref="Microsoft.Vbe.Interop.VBComponent"/> objects.
+    /// Each instance contains the type code and corresponding file extension.
+    /// </summary>
     public class VbComponentType
     {
         // Enum Instances
+        /// <summary>
+        /// Represents a "Class Module" in VBA.
+        /// </summary>
         public static VbComponentType ClassModule { get; } = new VbComponentType("vbext_ct_ClassModule", ".cls");
+        /// <summary>
+        /// Represents a standard "Module" in VBA.
+        /// </summary>
         public static VbComponentType StandardModule { get; } = new VbComponentType("vbext_ct_StdModule", ".bas");
+        /// <summary>
+        /// Represents a "Microsoft Excel Object" component (i.e. a worksheet component) in VBA.
+        /// </summary>
         public static VbComponentType Sheet { get; } = new VbComponentType("vbext_ct_Document", ".sht");
+        /// <summary>
+        /// Represents a "Form" component in VBA.
+        /// </summary>
         public static VbComponentType UserForm { get; } = new VbComponentType("vbext_ct_MSForm", ".frm");
 
-        // Set of Enum values
+        /// <summary>
+        /// Set of all possible component types.
+        /// </summary>
         public static ISet<VbComponentType> Values { get; } = CalculateComponentTypeSet();
         private static ISet<VbComponentType> CalculateComponentTypeSet()
         {
@@ -24,8 +42,14 @@ namespace ExcelVbaSync.Vba
             };
         }
 
-        // Data stored for each instance
+        /// <summary>
+        /// Code used by Interop to identify this component type.
+        /// </summary>
         public string VbCompTypeCode { get; }
+
+        /// <summary>
+        /// File extension to use when saving or reading this component from a file.
+        /// </summary>
         public string FileExt { get; }
 
         private VbComponentType(string vbCompTypeCode, string fileExt)
